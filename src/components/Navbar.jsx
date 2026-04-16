156a04200471a64b4bf9d2d9d1c4eb83a6be5213
trentproductionz-design <trentproductionz@gmail.com>
2026-04-16 16:26:45 -0400

    a lot

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -95,7 +95,10 @@ export default function Navbar() {
             onClick={closeMenu}
             aria-label="Close menu"
           >
-            Close
+            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round">
+              <line x1="18" y1="6" x2="6" y2="18" />
+              <line x1="6" y1="6" x2="18" y2="18" />
+            </svg>
           </button>
         </div>
 
