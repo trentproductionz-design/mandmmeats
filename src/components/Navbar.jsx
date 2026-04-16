b8d92dc1cdc741e20cebfad90ed0303f63011bd2
trentproductionz-design <trentproductionz@gmail.com>
2026-04-16 16:27:45 -0400

    a lot

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -89,17 +89,6 @@ export default function Navbar() {
       >
         <div className="mobile-nav-head">
           <span className="mobile-nav-kicker">Menu</span>
-          <button
-            type="button"
-            className="mobile-nav-close"
-            onClick={closeMenu}
-            aria-label="Close menu"
-          >
-            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round">
-              <line x1="18" y1="6" x2="6" y2="18" />
-              <line x1="6" y1="6" x2="18" y2="18" />
-            </svg>
-          </button>
         </div>
 
         <div className="mobile-nav-links">
